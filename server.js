12d08032398fbb15cabb406b5defcecf22252604
BiggJxny <montanasquad360@gmail.com>
2025-10-25 10:38:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,4 +45,5 @@ app.post("/chat", async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => console.log("Server is running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
